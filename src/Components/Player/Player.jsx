3f3c718e32452e088c471b5455756bf672a1044e
almamuntmn <almamuncool88@gmail.com>
2025-08-25 10:57:23 +0600

    added flag

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,3 +1,5 @@
+import { IoFlagSharp } from "react-icons/io5";
+
 const Player = ({ player }) => {
 
     const { player_cover, player_img, name, country, category, price } = player
@@ -10,14 +12,17 @@ const Player = ({ player }) => {
                     <img className="w-8 h-8 object-cover rounded-full" src={player_img} alt="" />
                     <h1 className="text-lg font-semibold">{name}</h1>
                 </div>
-                <p className="mb-4">{country}</p>
+                <div className="flex gap-1 text-gray-400">
+                    <IoFlagSharp />
+                    <p className="mb-4 text-sm">{country}</p>
+                </div>
                 <hr className="border border-gray-300" />
             </div>
             <div className="mt-4 font-medium">
                 <h3 className="mb-3">Rating</h3>
                 <h3 className="mb-3">{category}</h3>
                 <div className="flex justify-between">
-                    <h3>Price: ${price}</h3>
+                    <h3 className="text-orange-300">Price: ${price}</h3>
                     <button className="text-sm border border-gray-300 py-1 px-2 rounded-md font-light">Choose Player</button>
                 </div>
             </div>
